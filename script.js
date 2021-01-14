fbd1d64ba528dfb8b69c8f461ff41311db9e84d4
Wandrei Martins <wandrei.martins@gmail.com>
2021-01-14 02:07:45 -0300

    Feature: formatação de botões/e suas logicas

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
 let criarGasto = () =>{
 
-let nomeGasto = document.getElementById('nomeGasto').value
-let nomeValor = document.getElementById('nomeValor').value
+  let nomeGasto = document.getElementById('nomeGasto').value
+  let nomeValor = document.getElementById('nomeValor').value
+  
+  
+  let divElemento = document.createElement('div')
+  divElemento.classList.add('divClasse')
+  let local = document.getElementById('despesas')
+  
+  
+  const qtdDesp = document.getElementsByClassName('despesas').length
+  
+  
+  local.appendChild(divElemento).innerHTML = `
+  <div class="despesas" id="desp${qtdDesp}">
+    <span>${nomeGasto}:</span><input type="number" id="nome${qtdDesp}">
+    <div onclick="removerReceita(${qtdDesp})" class="btn"> <span>x</span> </div>
+  </div>`
+  document.getElementById(`nome${qtdDesp}`).value = nomeValor
+  }
+  
+  
+  function removerDespesa(element){
+    const ElementoParaRemover = document.getElementById('desp'+element)
+    ElementoParaRemover.remove()
+  }
 
-if(nomeValor !== '' && nomeGasto !== '' ){
+ // --------------- inicio receita ---------------
 
-let divElemento = document.createElement('div')
-divElemento.classList.add('divBtn')
-let local = document.getElementById('despesas')
-let identidade = Math.random().toFixed(20)
-console.log(identidade)
-local.appendChild(divElemento).innerHTML = `
-<div class="RecDes">
-  <span>${nomeGasto}:</span><input type="number" id="nome${identidade}">
-  <div class="btn">
-   <span></span>
-   <span></span>
-</div>`
-document.getElementById(`nome${identidade}`).value = nomeValor
-}else{
-  alert('Preencha o nome e valor do campo de gastos.')
-}
-}
+ let criarReceita = () =>{
 
-  function criarReceita1() {
   let nomeReceita = document.getElementById('nomeReceita').value
   let nomeValorReceita = document.getElementById('nomeValorReceita').value
-
-  if(nomeValorReceita !== '' && nomeReceita !== '' ){  
-
-  let divE = document.createElement('div')
-  divE.classList.add('divBtn')
-  let localR = document.getElementById('receita')
-  let identidade = Math.random().toFixed(20)
-  localR.appendChild(divE).innerHTML = `
-  <div class="RecDes" id="item">
-  <span>${nomeReceita}:</span><input type="number" id="nomeR${identidade}">
-  <div class="btn" onclick="removerItem()">
-    <span></span>
-    <span></span>
- </div>`
-  document.getElementById(`nomeR${identidade}`).value = nomeValorReceita
-  }else{
-
-    alert('Preencha o nome e valor do campo da receita.')
-
+  
+  
+  let divElemento = document.createElement('div')
+  divElemento.classList.add('divClasse')
+  let local = document.getElementById('receita')
+  
+  
+  const qtdDesp = document.getElementsByClassName('receitas').length
+  
+  
+  local.appendChild(divElemento).innerHTML = `
+  <div class="receitas" id="desp${qtdDesp}">
+    <span>${nomeReceita}:</span><input type="number" id="nome${qtdDesp}">
+    <div onclick="removerReceita(${qtdDesp})" class="btn"> <span>x</span> </div>
+  </div>`
+  document.getElementById(`nome${qtdDesp}`).value = nomeValorReceita
   }
-}
-
-let removerItem = () =>{
-let item = document.getElementById('item')
-item = item.outerHTML
-item = item.slice(73, 105)
-console.log(item)
-console.log(Math.random().toFixed(20))
-// let apagar = document.getElementById(item)
-// console.log(apagar)
-
-}
+  
+  
+  function removerReceita(element){
+    const ElementoParaRemover = document.getElementById('desp'+element)
+    ElementoParaRemover.remove()
+  }
 
 
 
